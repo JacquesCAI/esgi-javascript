cdbeabd6a2b28c58064be60df8bdd513e386644d
Jacques CAI <jacques.cai1999@gmail.com>
2020-10-22 12:31:03 +0200

    Exo 1

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -1,128 +1,73 @@
 function ucfirst(chaine) {
-  if (typeof chaine != "string" || chaine === "") return "";
+  if (typeof chaine !== "string" || chaine === "") return "";
   return chaine.charAt(0).toUpperCase() + chaine.slice(1);
 }
 
 function capitalize(chaine) {
-  if (typeof chaine != "string" || chaine === "") return "";
-
-  let str = "";
-  let words = chaine.split(" ");
-  for (let i = 0; i < words.length; i++) {
-    if (i > 0) {
-      str += " ";
-    }
-    str += ucfirst(words[i].toLowerCase());
-  }
-  return str;
+  if (typeof chaine !== "string" || chaine === "") return "";
+  return chaine.split(' ').map(word => ucfirst(word.toLowerCase())).join(' ');
 }
 
 function camelCase(chaine) {
-  if (typeof chaine != "string" || chaine === "") return "";
-
-  let str = "";
-  let string = chaine.replace("_", " ");
-  let words = string.split(" ");
-  for (let i = 0; i < words.length; i++) {
-    str += ucfirst(words[i].toLowerCase());
-  }
-  return str;
+  if (typeof chaine !== "string" || chaine === "") return "";
+  return capitalize(chaine).replace(/\W/g, '');
 }
 
 function snake_case(chaine) {
-  if (typeof chaine != "string" || chaine === "") return "";
-
-  let str = "";
-  let words = chaine.split(" ");
-  for (let i = 0; i < words.length; i++) {
-    if (i > 0) {
-      str += "_";
-    }
-    str += words[i].toLowerCase();
-  }
-  return str;
+  if (typeof chaine !== "string" || chaine === "") return "";
+  return chaine.toLowerCase().replace(/\W/g, '_');
 }
 
 function leet(chaine) {
-  if (typeof chaine != "string" || chaine === "") return "";
-  const toCrypt = {
-    A: 4,
-    E: 3,
-    I: 1,
-    O: "0",
-    U: "(_)",
-    Y: 7,
-  };
-
-  for (let key in toCrypt) {
-    chaine = chaine.replace(new RegExp(key, "g"), toCrypt[key]);
-    chaine = chaine.replace(new RegExp(key.toLowerCase(), "g"), toCrypt[key]);
-  }
-  return chaine;
+  if (typeof chaine !== "string" || chaine === "") return "";
+  return chaine.replace(/[aeiouy]/gi, function($e) {
+      switch (e.toLowerCase()) {
+          case 'a':
+              return 4;
+          case 'e':
+              return 3;
+          case 'i':
+              return 1;
+          case 'o':
+              return 0;
+          case 'u':
+              return '(_)';
+          case 'y':
+              return 7;
+      }
+  });
 }
-
-function prop_access(obj, props) {
-  if (obj === "undefined") return obj;
-  if (typeof props !== "string" && props !== "") return obj;
-
-  let access = props.split(".");
-  let propPath = access[0];
-  let value = obj;
-
-  for (let i = 0; i < access.length; i++) {
-    propPath += `.${access[i]}`;
-
-    if (!value[access[i]]) {
-      return `${obj} don't exist`;
-    }
-    value = value[access[i]];
-  }
-
-  return value;
-}
-Z;
-
-let prairie = {
-  animal: {
-    type: {
-      name: "chien",
-    },
-  },
-};
 
 function verlan(chaine) {
-  if (typeof chaine != "string" || chaine === "") return "";
-
-  return chaine
-    .split(" ")
-    .map((word) => {
-      return word.split("").reverse().join("");
-    })
-    .join(" ");
+  if (typeof chaine !== "string" || chaine === "") return "";
+  return chaine.split(' ').map(w => w.split('').reverse().join('')).join(' ');
 }
 
 function yoda(chaine) {
-  if (typeof chaine != "string" || chaine === "") return "";
-
-  return chaine.split(" ").reverse().join(" ");
+  if (typeof chaine !== "string" || chaine === "") return "";
+  return chaine.split(' ').reverse().join(' ');
 }
 
-function vig(key, string) {
-    if (typeof key !== "string" && key !== "") return ""
-    if (typeof string !== "string" && string !== "") return ""
+function vig(str, code) {
+  while (code.length < str.length) {
+      code += code;
+  }
+  let codeIndex = 0;
+  return str.split("").map(function(car) {
+      car = car.toLowerCase();
+      const carCode = car.charCodeAt(0) - "a".charCodeAt(0);
 
-    const msgLength = string.length
-    const keyLength = key.length
+      if (carCode < 0 || carCode > 25) return car;
+      const codeCode = code[codeIndex++].charCodeAt(0) - "a".charCodeAt(0);
 
-    let cryptage = "";
+      const encodedCode = (carCode + codeCode) % 26;
 
-    for (let i = 1; i < msgLength; i++) {
-        let charCode = string.charCodeAt(i-1)-65
-        let decalage = key.charCodeAt((i-1)%keyLength)-65
-        let char = String.fromCharCode(65+(charCode+decalage)%26)
+      return String.fromCharCode(encodedCode + 'a'.charCodeAt(0));
+  }).join('');
+}
 
-        cryptage += char
-    }
-
-    return cryptage
+function prop_access(obj, path) {
+  return path.split('.').reduce((prev, curr) => {
+      return prev ? prev[curr] : null
+  }, obj) || path + " not exist";
 }
